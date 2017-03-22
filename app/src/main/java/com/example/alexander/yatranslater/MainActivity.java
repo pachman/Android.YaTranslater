9c683b406fd6cac0dbaa2ffae1a3c17be112e72e
pachman <as-sidorov.exe@ya.ru>
2017-03-22 22:45:07 +0300

    use AsyncTask, ProgressBar, icon

--- app/src/main/java/com/example/alexander/yatranslater/MainActivity.java
+++ app/src/main/java/com/example/alexander/yatranslater/MainActivity.java
@@ -1,23 +1,22 @@
 package com.example.alexander.yatranslater;
 
-import android.support.design.widget.TabLayout;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-   
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-  
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,18 +40,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tabs);
         tabLayout.setupWithViewPager(mViewPager);
+
+        TabLayout tabLayout2 = (TabLayout) findViewById(R.id.bottom_tabs);
+        tabLayout2.setupWithViewPager(mViewPager);
 
       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
       fab.setOnClickListener(new View.OnClickListener() {
@@ -65,31 +62,6 @@ public class MainActivity extends AppCompatActivity {
       
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    
-  
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -110,19 +82,16 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return getString(R.string.history);
                 case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
+                    return getString(R.string.favorite);
             }
             return null;
         }
@@ -154,12 +123,66 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        public View onCreateView(LayoutInflater inflater,
+                                 ViewGroup container,
+                                 Bundle savedInstanceState) {
+            int position = getArguments().getInt(ARG_SECTION_NUMBER);
+            View rootView;
+            switch (position) {
+                case 1:
+                    rootView = inflater.inflate(R.layout.history_fragment, container, false);
+                    final TextView textView = (TextView) rootView.findViewById(R.id.history_label);
+                    ImageButton imageButton = (ImageButton) rootView.findViewById(R.id.buttonSettings);
+                    imageButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            new TranslateTask(textView).execute();
+                        }
+                    });
+                    break;
+                case 2:
+                    rootView = inflater.inflate(R.layout.favorite_fragment, container, false);
+                    break;
+                default:
+                    rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                    break;
+            }
+
             return rootView;
         }
     }
 }
+
+
+class TranslateTask extends AsyncTask<Void, Void, Void> {
+
+    private TextView textView;
+
+    public TranslateTask(TextView textView){
+
+        this.textView = textView;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        textView.setText("Полез на крышу");
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+        textView.setText("Залез");
+    }
+}
+
